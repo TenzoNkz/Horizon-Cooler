de66b69ce173f2fecb01de83a687bd20459605d4
Tenzo Nakazato <125721690+TenzoNkz@users.noreply.github.com>
2026-08-22 00:31:48 +0700

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.horizon.cooler',
+  appName: 'Horizon Cooler',
+  webDir: 'www'
+};
+
+export default config;
