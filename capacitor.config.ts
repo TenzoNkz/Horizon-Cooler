521eb0105698bcb735135cb47cfb2be7cc0323cd
Tenzo Nakazato <125721690+TenzoNkz@users.noreply.github.com>
2026-08-22 00:28:49 +0700

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.horizon.cooler',
-  appName: 'Horizon Cooler',
-  webDir: 'www'
-};
-
-export default config;
